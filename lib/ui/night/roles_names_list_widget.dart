ee6e8ecd2d7bdda85a4ea3d5659958749d3b8a8c
Behzad Ayubifar <behzad13811374@gmail.com>
2024-05-22 07:57:15 -0700

    *fix self-slaughter

--- lib/ui/night/roles_names_list_widget.dart
+++ lib/ui/night/roles_names_list_widget.dart
@@ -45,13 +45,14 @@ class RoleNamesList extends StatelessWidget {
                       cacheExtent: _height / 1.64,
                       restorationId: 'night-page',
                       clipBehavior: Clip.antiAlias,
-                      itemCount: roleNames.values
-                          .where((role) => ![
-                                MyStrings.godfather,
-                                MyStrings.matador,
-                                MyStrings.saul,
-                                MyStrings.mafia,
-                              ].contains(role))
+                      itemCount: roleNames
+                          .values
+                          // .where((role) => ![
+                          //       MyStrings.godfather,
+                          //       MyStrings.matador,
+                          //       MyStrings.saul,
+                          //       MyStrings.mafia,
+                          //     ].contains(role))
                           .length,
                       itemBuilder: (context, index) {
                         return MyButton(
